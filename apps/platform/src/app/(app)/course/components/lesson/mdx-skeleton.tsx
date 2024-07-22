a78e00eb48c4062e9d8a24d688760c8b88537165
Ramu Narasinga <ramu@binlo.com.hk>
2024-07-22 22:50:09 +0100

    fix: build

--- apps/platform/src/app/(app)/course/components/lesson/mdx-skeleton.tsx
+++ apps/platform/src/app/(app)/course/components/lesson/mdx-skeleton.tsx
@@ -1,8 +1,8 @@
 export default function MdxSkeleton() {
     return (
       <div>
-        {Array.from(Array(10).keys()).map(() => (
-          <div className="animate-pulse">
+        {Array.from(Array(10).keys()).map((item, index) => (
+          <div className="animate-pulse" key={index}>
             <span className="bg-slate-200 dark:bg-slate-700 rounded h-[30px] block w-[35vw] mt-1"></span>
           </div>
         ))}
--- apps/platform/src/app/(app)/course/components/lesson/steps/skeleton.tsx
+++ apps/platform/src/app/(app)/course/components/lesson/steps/skeleton.tsx
@@ -3,8 +3,8 @@ import PendingIcon from "./icons/pending";
 export default function StepsSkeleton() {
   return (
     <div className="flex flex-col gap-2">
-      {Array.from(Array(4).keys()).map(() => (
-        <div className="animate-pulse">
+      {Array.from(Array(4).keys()).map((item, index) => (
+        <div className="animate-pulse" key={index}>
           <div className="aa cq du ia kt ph rd" aria-hidden="true"></div>
           <div className="bqa ab lx yx">
             <PendingIcon />
