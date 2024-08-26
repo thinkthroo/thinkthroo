49db5ae86aebada2998f02e7dc81575d1c99ca6e
Ramu Narasinga <ramu@binlo.com.hk>
2024-08-26 14:49:13 +0100

    fix: more changes around platform

--- apps/platform/src/app/(app)/(modules)/layout.tsx
+++ apps/platform/src/app/(app)/(modules)/layout.tsx
@@ -1,5 +1,6 @@
 import SiteHeader from "@/components/site/header";
 import SiteSidebar from "@/components/site/sidebar";
+import { TechNav } from "@/components/site/tech-nav";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function ModuleLayout({
@@ -13,7 +14,10 @@ export default function ModuleLayout({
             <main className="flex-1">
                 <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
                     <SiteSidebar />
-                    {children}
+                    <div className="flex flex-col py-4">
+                        <TechNav />
+                        {children}
+                    </div>
                 </div>
             </main>
         </>
