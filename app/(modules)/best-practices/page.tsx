1a58f6a3f02ec1a274424f929474e97674624f74
Ramu Narasinga <ramu@binlo.com.hk>
2024-09-09 23:32:43 +0100

    added courses into a lib/data

--- app/(modules)/best-practices/page.tsx
+++ app/(modules)/best-practices/page.tsx
@@ -19,8 +19,12 @@ import {
 } from "@/components/ui/select";
 import { NavTabs } from "@/components/interfaces/page/nav-tabs";
 import { Icons } from "@/components/icons";
+import { getBestPracticesCourses } from "@/lib/data/get-best-practices-course";
 
-export default function ArchitecturePage() {
+export default function BestPracticesPage() {
+
+  const bestPracticesCourses = getBestPracticesCourses()
+
   return (
     <div className="container relative">
       <PageHeader>
@@ -60,26 +64,13 @@ export default function ArchitecturePage() {
       <div className="grid flex-1 gap-12">
         <div className="grid flex-1 scroll-mt-20 items-start gap-10 md:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:gap-10">
           
-          <div className="relative flex flex-col overflow-hidden rounded-xl border shadow transition-all duration-200 ease-in-out hover:z-30">
-            <div className="items-center gap-2 relative z-20 flex justify-end border-b bg-card px-3 py-2.5 text-card-foreground">
-              <div className="flex items-center gap-1.5 pl-1 text-[13px] text-muted-foreground">
-                Components Structure
-              </div>
-              <div className="ml-auto flex items-center gap-2 [&amp;>form]:flex">
-                <button
-                  className="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border-input hover:text-accent-foreground h-6 rounded-[6px] border bg-transparent px-2 text-xs text-foreground shadow-none hover:bg-muted dark:text-foreground"
-                  type="button"
-                  aria-haspopup="dialog"
-                  aria-expanded="false"
-                  aria-controls="radix-:rhu:"
-                  data-state="closed"
-                >
-                  Practise
-                </button>
-                {/* <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-0 hidden h-4 md:flex"></div>
-                <Link
-                  href={`/course/components-structure-in-shadcn-ui/introduction`}
-                >
+        {
+            bestPracticesCourses.map(course => <div className="relative flex flex-col overflow-hidden rounded-xl border shadow transition-all duration-200 ease-in-out hover:z-30">
+              <div className="items-center gap-2 relative z-20 flex justify-end border-b bg-card px-3 py-2.5 text-card-foreground">
+                <div className="flex items-center gap-1.5 pl-1 text-[13px] text-muted-foreground">
+                  {course.concept.label}
+                </div>
+                <div className="ml-auto flex items-center gap-2 [&amp;>form]:flex">
                   <button
                     className="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border-input hover:text-accent-foreground h-6 rounded-[6px] border bg-transparent px-2 text-xs text-foreground shadow-none hover:bg-muted dark:text-foreground"
                     type="button"
@@ -88,36 +79,62 @@ export default function ArchitecturePage() {
                     aria-controls="radix-:rhu:"
                     data-state="closed"
                   >
-                    Read
+                    {course.chapters} Chapters
                   </button>
-                </Link> */}
-              </div>
-            </div>
-            <div className="relative z-10 [&amp;>div]:rounded-none [&amp;>div]:border-none [&amp;>div]:shadow-none">
-              <div className="bg-card text-card-foreground shadow">
-                <div className="flex flex-col space-y-1.5 p-6">
-                  <h3 className="font-semibold leading-none tracking-tight">
-                    Components Structure In Shadcn-ui/ui
-                  </h3>
-                  <p className="text-sm text-muted-foreground">
-                    Learn how the components are organized in Shadcn-ui/ui
-                  </p>
+                  <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-0 hidden h-4 md:flex"></div>
+                  <button
+                    className="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border-input hover:text-accent-foreground h-6 rounded-[6px] border bg-transparent px-2 text-xs text-foreground shadow-none hover:bg-muted dark:text-foreground"
+                    type="button"
+                    aria-haspopup="dialog"
+                    aria-expanded="false"
+                    aria-controls="radix-:rhu:"
+                    data-state="closed"
+                  >
+                    {course.lessons} Lessons
+                  </button>
+                  <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-0 hidden h-4 md:flex"></div>
+                  <Link
+                    href={course.slug}
+                  >
+                    <button
+                      className="inline-flex items-center justify-center whitespace-nowrap font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border-input hover:text-accent-foreground h-6 rounded-[6px] border bg-transparent px-2 text-xs text-foreground shadow-none hover:bg-muted dark:text-foreground"
+                      type="button"
+                      aria-haspopup="dialog"
+                      aria-expanded="false"
+                      aria-controls="radix-:rhu:"
+                      data-state="closed"
+                    >
+                      Read
+                    </button>
+                  </Link>
                 </div>
-                <div className="p-6 pt-0">
-                  <div className="grid grid-cols-3 gap-2">
-                    <div className="flex items-center">
-                      <Icons.tag />
-                      next.js
-                    </div>
-                    <div className="flex items-center">
-                      <Icons.tag />
-                      shadcn-ui/ui
+              </div>
+              <div className="relative z-10 [&amp;>div]:rounded-none [&amp;>div]:border-none [&amp;>div]:shadow-none">
+                <div className="bg-card text-card-foreground shadow">
+                  <div className="flex flex-col space-y-1.5 p-6">
+                    <h3 className="font-semibold leading-none tracking-tight">
+                      {course.title}
+                    </h3>
+                    <p className="text-sm text-muted-foreground">
+                      {course.description}
+                    </p>
+                  </div>
+                  <div className="p-6 pt-0">
+                    <div className="grid grid-cols-3 gap-2">
+                      {
+                        course.tags.map(tag => <div className="flex items-center">
+                            <Icons.tag />
+                            {tag}
+                          </div>
+                        )
+                      }
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+            )
+          }
 
         </div>
       </div>
