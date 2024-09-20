a2d83fff90d7af5e15459de4700c77bff964de63
Turbobot <turbobot@vercel.com>
2024-09-20 20:44:42 +0100

    feat(create-turbo): apply official-starter transform

--- app/(modules)/course/layout.tsx
+++
@@ -1,9 +0,0 @@
-import CommonCourseLayout, { CommonCourseLayoutProps } from "@/components/layouts/course";
-
-export default function Courselayout({ children }: CommonCourseLayoutProps) {
-    return (
-        <CommonCourseLayout>
-            {children}
-        </CommonCourseLayout>
-    )
-}
