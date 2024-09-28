de706ec2e9ebb9f524dc9e3f127f93e3e4aa413a
Ramu Narasinga <ramu@binlo.com.hk>
2024-09-28 14:34:25 +0100

    www updates

--- apps/www/app/(modules)/page.tsx
+++ apps/www/app/(modules)/page.tsx
@@ -31,10 +31,9 @@ export default function RootPage() {
   return (
     <div className="container relative">
       <PageHeader>
-        <PageHeaderHeading>Architecture</PageHeaderHeading>
+        <PageHeaderHeading>Learn the Large Codebase Architecture</PageHeaderHeading>
         <PageHeaderDescription>
-          Learn the architectural concepts, build your confidence and 10x your
-          coding skills.
+          We study large open-source projects and provide free architectural guides.
         </PageHeaderDescription>
         <PageActions>
           <Button asChild size="sm">
@@ -52,65 +51,58 @@ export default function RootPage() {
         </PageActions>
       </PageHeader>
 
-      <div className="py-24 sm:py-32">
-        <div className="px-6 max-w-7xl mx-auto">
-          <div className="sm:mt-24 flow-root mt-16">
-            <div className="ring-gray-900/10 ring-inset custom-ring p-2 bg-gray-900/5 rounded-lg -m-2">
-              <img
-                alt="App screenshot"
-                src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
-                width="2432"
-                height="1442"
-                className="aej bcf bcp bdl"
-              />
-            </div>
-          </div>
-        </div>
+      <div className="ring-gray-900/10 ring-inset custom-ring p-2 bg-gray-900/5 rounded-lg -m-2">
+        <Image
+          alt="App screenshot"
+          src="/architecture.png"
+          width="2432"
+          height="1442"
+          className="aej bcf bcp bdl"
+        />
       </div>
 
-      <div className="px-6 max-w-7xl mx-auto">
-        <div className="gap-y-12 gap-x-8 sm:gap-y-14 sm:gap-x-10 sm:grid-cols-6 sm:max-w-lg grid-cols-4 max-w-lg grid items-center mx-auto">
+      <div className="max-w-7xl mx-auto px-4 py-8 md:py-12 md:pb-10 lg:py-14 lg:pb-10 mt-24">
+        <div className="lg:grid-cols-5 lg:max-w-none lg:mx-0 sm:row-gap-14 sm:gap-x-10 sm:grid-cols-6 sm:max-w-3xl gap-y-12 gap-x-8 items-center grid-cols-4 max-w-lg grid mx-auto">
           <img
             alt="Transistor"
             src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
             width="158"
             height="48"
-            className="object-contain w-full max-h-12 col-span-2"
+            className="object-contain w-full max-h-12 col-span-2 lg:col-span-1"
           />
           <img
             alt="Reform"
             src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
             width="158"
             height="48"
-            className="object-contain w-full max-h-12 col-span-2"
+            className="object-contain w-full max-h-12 col-span-2 lg:col-span-1"
           />
           <img
             alt="Tuple"
             src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
             width="158"
             height="48"
-            className="object-contain w-full max-h-12 col-span-2"
+            className="object-contain w-full max-h-12 col-span-2 lg:col-span-1"
           />
           <img
             alt="SavvyCal"
             src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
             width="158"
             height="48"
-            className="object-contain w-full max-h-12 col-span-2"
+            className="object-contain w-full max-h-12 col-span-2 lg:col-span-1"
           />
           <img
             alt="Statamic"
             src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
             width="158"
             height="48"
-            className="object-contain w-full max-h-12 col-span-2"
+            className="object-contain w-full max-h-12 col-span-2 lg:col-span-1"
           />
         </div>
         <div className="justify-center flex mt-16">
-          <div className="ring-gray-900/10 ring-inset custom-ring text-gray-600 leading-6 text-sm leading-5 py-1.5 px-4 rounded-full relative">
+          <div className="ring-1 ring-gray-900/10 ring-inset shadow-sm text-gray-600 leading-6 text-sm leading-5 py-1.5 px-4 rounded-full relative">
             <span className="hidden md:inline">
-              Transistor saves up to $40,000 per year, per employee by working
-              with us.
+             We improved our code quality referencing open-source.
             </span>
             <a href="#" className="text-black font-semibold">
               <span aria-hidden="true" className="absolute inset-0"></span> Read our
@@ -120,18 +112,16 @@ export default function RootPage() {
         </div>
       </div>
 
-      <div className="mt-0 sm:mt-56 px-6 max-w-7xl mt-32 mx-auto">
+      <div className="lg:px-8 px-6 max-w-7xl mt-24 mx-auto">
         <div className="max-w-xl mx-auto">
           <h2 className="text-black font-semibold text-base leading-6">
-            Deploy faster
+            Build a maintainable codebase
           </h2>
           <p className="mt-2 text-gray-900 font-bold text-lg leading-9 sm:text-2xl sm:leading-10 tracking-tight">
-            Everything you need to deploy your app
+            Study how large open-source projects architect their codebase
           </p>
           <p className="mt-6 text-gray-600 text-base leading-8">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis.
-            Suspendisse eget egestas a elementum pulvinar et feugiat blandit at.
-            In mi viverra elit nunc.
+            Learn how to structure your codebase, decide the tooling required, follow the best practices.
           </p>
         </div>
         <div className="mt-16 max-w-xl mt-16 mx-auto">
@@ -157,8 +147,7 @@ export default function RootPage() {
                 Tooling
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Tools such as ESLint that help you write, test, and manage their code more easily and efficiently.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -183,8 +172,7 @@ export default function RootPage() {
                 Project Structure
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Project Structure in a large open-source codebase organizes files and directories to enhance clarity and navigation for developers exploring the project.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -210,8 +198,7 @@ export default function RootPage() {
                 Components Structure
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Components Structure defines how reusable UI elements are organized within an open-source project, promoting modularity and ease of integration.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -239,8 +226,7 @@ export default function RootPage() {
                 API Layer
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                The API Layer serves as a bridge in large codebases, enabling smooth communication between the front-end and back-end components through well-defined interfaces.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -265,8 +251,7 @@ export default function RootPage() {
                 State Management
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                State Management in large open-source projects outlines how data is handled and shared across components, ensuring a consistent experience for users.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -292,8 +277,7 @@ export default function RootPage() {
                 Testing
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Testing in open-source codebases involves creating and running tests to confirm that various parts of the application function correctly, ensuring reliability.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -321,8 +305,7 @@ export default function RootPage() {
                 Error Handling
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Error Handling strategies in large codebases help developers anticipate, manage, and provide feedback on errors to maintain stability and user experience.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -347,8 +330,7 @@ export default function RootPage() {
                 Security
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Security measures in open-source projects focus on protecting the codebase from vulnerabilities and unauthorized access, which is vital for community trust.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -373,8 +355,7 @@ export default function RootPage() {
                 Performance
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Performance optimization in large codebases aims to enhance speed and responsiveness, improving user satisfaction and efficiency.
               </dd>
             </div>
             <div className="pl-16 relative">
@@ -401,14 +382,14 @@ export default function RootPage() {
                 Deployment
               </dt>
               <dd className="text-gray-600 text-base leading-6 mt-2">
-                Morbi viverra dui mi arcu sed. Tellus semper adipiscing
-                suspendisse semper morbi. Odio urna massa nunc massa.
+                Deployment in the context of open-source projects refers to the process of making the application accessible to users by moving it from a development environment to production.
               </dd>
             </div>
           </dl>
         </div>
+      </div>
 
-        <div className="pb-0 md:pb-32 md:pl-8 md:pr-8 md:max-w-7xl pt-12 sm:pb-24 px-6 max-w-xl mx-auto">
+      <div className="pb-0 md:pb-32 md:pl-8 md:pr-8 md:max-w-7xl pt-12 sm:pb-24 px-6 max-w-xl mx-auto">
           <h2 className="text-gray-900 font-bold text-xl leading-10 tracking-tight">
             Frequently asked questions
           </h2>
@@ -462,95 +443,6 @@ export default function RootPage() {
               </dd>
             </div>
           </dl>
-        </div>
-      </div>
-
-      <div className="md:pl-8 md:pr-8 pl-6 pr-6 mt-32 z-[-10] relative">
-        <div className="relative isolate overflow-hidden bg-white py-16 sm:py-24 lg:py-32">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
-            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
-              <div className="max-w-xl lg:max-w-lg">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  Subscribe to our newsletter.
-                </h2>
-                <p className="mt-4 text-lg leading-8 text-gray-700">
-                  Nostrud amet eu ullamco nisi aute in ad minim nostrud
-                  adipisicing velit quis. Duis tempor incididunt dolore.
-                </p>
-                <div className="mt-6 flex max-w-md gap-x-4">
-                  <label for="email-address" className="sr-only">
-                    Email address
-                  </label>
-                  <input
-                    id="email-address"
-                    name="email"
-                    type="email"
-                    autocomplete="email"
-                    required
-                    className="min-w-0 flex-auto rounded-md border-0 bg-gray-200 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                    placeholder="Enter your email"
-                  />
-                  <button
-                    type="submit"
-                    className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    Subscribe
-                  </button>
-                </div>
-              </div>
-              <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
-                <div className="flex flex-col items-start">
-                  <div className="rounded-md bg-gray-200 p-2 ring-1 ring-gray-300">
-                    <svg
-                      className="h-6 w-6 text-gray-900"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                      />
-                    </svg>
-                  </div>
-                  <dt className="mt-4 font-semibold text-gray-900">
-                    Weekly articles
-                  </dt>
-                  <dd className="mt-2 leading-7 text-gray-600">
-                    Non laboris consequat cupidatat laborum magna. Eiusmod non
-                    irure cupidatat duis commodo amet.
-                  </dd>
-                </div>
-                <div className="flex flex-col items-start">
-                  <div className="rounded-md bg-gray-200 p-2 ring-1 ring-gray-300">
-                    <svg
-                      className="h-6 w-6 text-gray-900"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      aria-hidden="true"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002"
-                      />
-                    </svg>
-                  </div>
-                  <dt className="mt-4 font-semibold text-gray-900">No spam</dt>
-                  <dd className="mt-2 leading-7 text-gray-600">
-                    Officia excepteur ullamco ut sint duis proident non
-                    adipisicing. Voluptate incididunt anim.
-                  </dd>
-                </div>
-              </dl>
-            </div>
-          </div>
-        </div>
       </div>
 
       <div className="md:pl-8 md:pr-8 pl-6 pr-6 max-w-[80rem] mt-32 mx-auto">
